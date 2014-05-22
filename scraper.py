9fdf5dd4d6ae39f70f055a943ecb96aa9b29f38c
Andy Lulham <a.lulham@gmail.com>
2014-05-22 17:52:42 +0100

    Initial commit of scraper

--- scraper.py
+++ scraper.py
@@ -1,23 +1,28 @@
-# This is a template for a Python scraper on Morph (https://morph.io)
-# including some code snippets below that you should find helpful
+import urllib2, urllib, json, csv
+import scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
-# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
-# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-# has at least a table called data.
+headers = {'X-Requested-With': 'XMLHttpRequest'}
+data = urllib.urlencode({
+    'Position': '0,0',
+    'Bounds': '-100,-100,100,100',
+})
+url = 'https://www.greggs.co.uk/home/shop-finder'
+
+# run the request
+raw_res = urllib2.urlopen(urllib2.Request(
+    url,
+    data=data,
+    headers=headers,
+)).read()
+
+# jsonify and listify the output
+res = [{'id': id_, 'name': x['Name'], 'lat': x['Latitude'], 'long': x['Longitude'], 'phone': x['Telephone'], 'addr': x['HTMLAddress']} for id_, x in json.loads(raw_res).items()]
+
+# save it
+scraperwiki.sqlite.save(unique_keys=['id'], data=res)
+
+# with open('out.csv', 'wb') as f:
+#     w = csv.DictWriter(f, res[0].keys())
+#     w.writeheader()
+#     w.writerows(res)
